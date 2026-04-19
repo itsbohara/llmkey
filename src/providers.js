7425bb330acfd66956a50bd9f6651d437e086179
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2026-04-19 08:56:20 +0545

    feat: add provider resolution

--- src/providers.js
+++ src/providers.js
@@ -1,1 +1,15 @@
-module.exports = {};
+const PROVIDERS = {
+  openai: 'OPENAI_API_KEY',
+  fireworks: 'FIREWORKS_API_KEY',
+  nvidia: 'NVIDIA_API_KEY',
+};
+
+function resolveEnvVar(arg) {
+  return PROVIDERS[arg] || arg;
+}
+
+function isKnownProvider(arg) {
+  return arg in PROVIDERS;
+}
+
+module.exports = { PROVIDERS, resolveEnvVar, isKnownProvider };
