18cf1b9b7f8bbb6bd4164f69eab40a39f59bb882
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2026-04-19 09:01:31 +0545

    feat: add remove command

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const { resolveEnvVar } = require('../providers');
+const { read, write } = require('../store');
+
+function remove(providerArg) {
+  const envVar = resolveEnvVar(providerArg);
+  const keys = read();
+
+  if (!(envVar in keys)) {
+    console.log(`No key found for ${envVar}`);
+    return;
+  }
+
+  delete keys[envVar];
+  write(keys);
+  console.log(`✓ ${envVar} removed.`);
+}
+
+module.exports = { remove };
